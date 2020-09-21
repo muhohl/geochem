f190edd10663bea2a340632ea83465e9870c3b06
muhohl <max.hohl@utas.edu.au>
2020-09-21 22:41:27 +1000

    Initial commit

--- R/Random_Number_Imputer.R
+++ R/Random_Number_Imputer.R
@@ -1,29 +1,33 @@
 # Random Number Imputer
 # Function that imputes a random number below the detection limit.
 
-
-Random_Number_Imputer <- function(data, 
-                                  columns, 
-                                  split_symbol = "<", 
+Random_Number_Imputer <- function(data,
+                                  columns,
+                                  split_symbol = "<",
                                   digits = 6) {
     require(purrr)
     require(stringr)
+    require(tibble)
+    require(dplyr)
+    require(magrittr)
+
+
     laser_data <- data[columns]
-    laser_split <- tibble(1:nrow(laser_data), 
+    laser_split <- tibble::tibble(1:nrow(laser_data),
                           nrow(laser_data):1)
-    
+
     for (i in 1:ncol(laser_data)) {
         for (j in 1:nrow(laser_data)) {
             if (stringr::str_detect(laser_data[j, i], split_symbol)) {
-                laser_split <- laser_data[[i]] %>% 
+                laser_split <- laser_data[[i]] %>%
                     stringr::str_split(split_symbol, simplify = TRUE)
-                
-                for (k in nrow(laser_split):1) { 
-                    if (laser_split[[k, 1]] == "" | 
-                        is.na(laser_split[[k, 1]])) { 
-                        laser_split[[k, 1]] <- 
-                            round(runif(n = 1, min = 0, 
-                                        max = as.numeric(laser_split[k, 2])), 
+
+                for (k in nrow(laser_split):1) {
+                    if (laser_split[[k, 1]] == "" |
+                        is.na(laser_split[[k, 1]])) {
+                        laser_split[[k, 1]] <-
+                            round(stats::runif(n = 1, min = 0,
+                                        max = as.numeric(laser_split[k, 2])),
                                   digits = digits)
                     }
                 }
@@ -31,10 +35,10 @@ Random_Number_Imputer <- function(data,
             }
         }
     }
-    
-    laser_data <- laser_data %>% 
-        purrr::map(as.character) %>% 
+
+    laser_data <- laser_data %>%
+        purrr::map(as.character) %>%
         purrr::map(as.numeric)
     data[columns] <- laser_data
-    return(data) 
+    return(data)
 }
