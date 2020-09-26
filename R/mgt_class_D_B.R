cddbef8181cce43e2ea896470fba6da3a1507d92
muhohl <max.hohl@utas.edu.au>
2020-09-26 21:43:32 +1000

    Updated mgt_class_D_B now it should work properly with a ggplot.

--- R/mgt_class_D_B.R
+++ R/mgt_class_D_B.R
@@ -1,41 +1,51 @@
+#' Magnetite Classification Template after Dupuis&Beaudoin
+#'
+#'
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+#'
 mgt_class_D_B <- function() {
 
-    Skarn <- tibble(x = c(0.005, 1.5, 1.25, 0.38, 0.145,
-                          0.085, 0.047, 0.047, 0.005),
-                    y = c(3, 3, 0.95, 0.71, 0.35,
-                          0.4, 0.215, 0.12, 0.12))
-    Porphyry <- tibble(x = c(1.25, 0.38, 0.145, 0.151, 0.9),
-                       y = c(0.95, 0.71, 0.35, 0.14, 0.135))
-    Kiruna <- tibble(x = c(0.9, 0.7, 0.12, 0.151),
-                     y = c(0.135, 0.035, 0.091, 0.14))
-    IOCG <- tibble(x = c(0.145, 0.151, 0.12, 0.047, 0.047, 0.085),
-                   y = c(0.35, 0.14, 0.091, 0.035, 0.215, 0.4))
-    BIF <- tibble(x = c(0.047, 0.025, 0.0105, 0.047),
-                  y = c(0.035, 0.022, 0.12, 0.12))
-    FeTiV <- tibble(x = c(1.5, 20, 20, 0.55, 0.9, 1.25),
-                    y = c(3, 3, 0.01, 0.01, 0.135, 0.95))
+    Skarn <- tibble::tibble(x = c(0.005, 1.5, 1.25, 0.38, 0.145,
+                                  0.085, 0.047, 0.047, 0.005),
+                            y = c(3, 3, 0.95, 0.71, 0.35,
+                                  0.4, 0.215, 0.12, 0.12))
+    Porphyry <- tibble::tibble(x = c(1.25, 0.38, 0.145, 0.151, 0.9),
+                               y = c(0.95, 0.71, 0.35, 0.14, 0.135))
+    Kiruna <- tibble::tibble(x = c(0.9, 0.7, 0.12, 0.151),
+                             y = c(0.135, 0.035, 0.091, 0.14))
+    IOCG <- tibble::tibble(x = c(0.145, 0.151, 0.12, 0.047, 0.047, 0.085),
+                           y = c(0.35, 0.14, 0.091, 0.035, 0.215, 0.4))
+    BIF <- tibble::tibble(x = c(0.047, 0.025, 0.0105, 0.047),
+                          y = c(0.035, 0.022, 0.12, 0.12))
+    FeTiV <- tibble::tibble(x = c(1.5, 20, 20, 0.55, 0.9, 1.25),
+                            y = c(3, 3, 0.01, 0.01, 0.135, 0.95))
 
-    mgt_class_template <-
-        geom_polygon(data = Skarn, aes(x, y), fill = "green",
-                     alpha = 0.1, color = "black") +
-        geom_polygon(data = Porphyry, aes(x, y), fill = "blue",
-                     alpha = 0.1, color= "black") +geom_polygon(data = Kiruna, aes(x, y), fill = "yellow",
-                                                                alpha = 0.1, color = "black") +
-        geom_polygon(data = IOCG, aes(x, y), fill = "orange",
-                     alpha = 0.1, color = "black") +
-        geom_polygon(data = BIF, aes(x, y), fill = "red",
-                     alpha = 0.1, color = "black") +
-        geom_polygon(data = FeTiV, aes(x, y), fill = "sienna",
-                     alpha = 0.1, color = "black") +
-        annotate("text", x = 0.01, y = 2, label = "Skarn") +
-        annotate("text", x = 5, y = 2, label = "Fe-Ti, V") +
-        annotate("text", x = 0.02, y = 0.1, label = "BIF") +
-        annotate("text", x = 0.1, y = 0.3, label = "IOCG") +
-        annotate("text", x = 0.55, y = 0.2, label = "Porphyry") +
-        annotate("text", x = 0.55, y = 0.12, label = "Kiruna") +
-        scale_x_log10() +
-        scale_y_log10() +
-        labs(x = "Ti + V (wt%)", y = "Al + Mn (wt%)")
+    mgt_class_template <- list(
+        ggplot2::geom_polygon(data = Skarn, ggplot2::aes(x, y), fill = "green",
+                              alpha = 0.1, color = "black"),
+        ggplot2::geom_polygon(data = Porphyry, ggplot2::aes(x, y), fill = "blue",
+                              alpha = 0.1, color= "black"),
+        ggplot2::geom_polygon(data = Kiruna, ggplot2::aes(x, y), fill = "yellow",
+                              alpha = 0.1, color = "black"),
+        ggplot2::geom_polygon(data = IOCG, ggplot2::aes(x, y), fill = "orange",
+                              alpha = 0.1, color = "black"),
+        ggplot2::geom_polygon(data = BIF, ggplot2::aes(x, y), fill = "red",
+                              alpha = 0.1, color = "black"),
+        ggplot2::geom_polygon(data = FeTiV, ggplot2::aes(x, y), fill = "sienna",
+                              alpha = 0.1, color = "black"),
+        ggplot2::annotate("text", x = 0.01, y = 2, label = "Skarn"),
+        ggplot2::annotate("text", x = 5, y = 2, label = "Fe-Ti, V"),
+        ggplot2::annotate("text", x = 0.02, y = 0.1, label = "BIF"),
+        ggplot2::annotate("text", x = 0.1, y = 0.3, label = "IOCG"),
+        ggplot2::annotate("text", x = 0.55, y = 0.2, label = "Porphyry"),
+        ggplot2::annotate("text", x = 0.55, y = 0.12, label = "Kiruna"),
+        ggplot2::scale_x_log10(),
+        ggplot2::scale_y_log10(),
+        ggplot2::labs(x = "Ti + V (wt%)", y = "Al + Mn (wt%)"))
 
     return(mgt_class_template)
 }
