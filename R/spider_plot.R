02ba0209a59d5005ef25f3ec9c49332367c758c8
muhohl <max.hohl@utas.edu.au>
2020-10-28 21:33:28 +1100

    Spider Plots should be good to go.

--- R/spider_plot.R
+++ R/spider_plot.R
@@ -1,13 +1,37 @@
+#' Spider Plot faceted
+#'
+#' Produces spider plots highlighting different groups per facet and compares them
+#' to the remaining groups.
+#'
+#' @param data
+#' Dataframe
+#' @param elements
+#' Select the elements to include in the plot as vector.
+#' @param group
+#' Select the grouping of the data set as character variable. This needs to be the
+#' name of a column in the data frame.
+#' @param levels
+#' Select sequence of elements.
+#' @param ribbon
+#' If TRUE the fourth and first quantile are summarised to a ribbon, if FALSE
+#' single lines for those samples are plotted.
+#' @param ncol
+#' Number of facet columns.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 spider_plot <- function(data,
+                        elements,
                         group,
-                        columns,
-                        levels,
+                        levels = elements,
                         ribbon = TRUE,
                         ncol = NULL) {
 
-    By_Group <- function(data, group, columns, levels) {
+    By_Group <- function(data, group, elements, levels) {
         quo_group <-  ggplot2::sym(group)
-        data <- data %>% dplyr::select(columns)
+        data <- data %>% dplyr::select(elements)
         ncols <- ncol(data)
         data %>%
             tidyr::pivot_longer(2:ncols, names_to = "elements", values_to = "ppm") %>%
@@ -17,8 +41,8 @@ spider_plot <- function(data,
                                      levels = levels))
     }
 
-    By_Gather <- function(data, columns, levels) {
-        data <- data %>% dplyr::select(columns)
+    By_Gather <- function(data, elements, levels) {
+        data <- data %>% dplyr::select(elements)
         ncols <- ncol(data)
         data %>%
             dplyr::mutate(ID = 1: dplyr::n()) %>%
@@ -27,89 +51,89 @@ spider_plot <- function(data,
                                      levels = levels))
     }
 
-    By_Gather_Ribbon <- function(data, group, columns, levels) {
+    By_Gather_Ribbon <- function(data, group, elements, levels) {
         quo_group <- dplyr::sym(group)
         By_Gather(data = data,
-                  columns = columns,
+                  elements = elements,
                   levels = levels) %>%
-            group_by(elements, !! quo_group) %>%
-            summarise(min = quantile(ppm, na.rm = TRUE)[[2]],
-                      max = quantile(ppm, na.rm = TRUE)[[4]],
-                      median = median(ppm, na.rm = TRUE)) %>%
-            mutate(ID = 1:n())
+            dplyr::group_by(elements, !! quo_group) %>%
+            dplyr::summarise(min = stats::quantile(ppm, na.rm = TRUE)[[2]],
+                      max = stats::quantile(ppm, na.rm = TRUE)[[4]],
+                      median = stats::median(ppm, na.rm = TRUE)) %>%
+            dplyr::mutate(ID = 1: dplyr::n())
     }
 
-    quo_group <- sym(group)
+    quo_group <- ggplot2::sym(group)
 
     data1 <- By_Group(data = data,
                       group = group,
-                      columns = columns,
+                      elements = elements,
                       levels = levels)
 
     data2 <- data1
-    data2 <- rename(data2, "XXX" = 1)
+    data2 <- dplyr::rename(data2, "XXX" = 1)
 
     if (ribbon == FALSE) {
 
         By_Gather(data = data,
-                  columns = columns,
+                  elements = elements,
                   levels = levels) %>%
-            ggplot(aes(elements,
+            ggplot2::ggplot(ggplot2::aes(elements,
                        ppm,
                        group = ID,
                        color = !! quo_group)) +
-            geom_line(alpha = 0.2) +
-            geom_line(data = data2, aes(elements,
+            ggplot2::geom_line(alpha = 0.2) +
+            ggplot2::geom_line(data = data2, ggplot2::aes(elements,
                                         Median,
                                         group = XXX),
                       alpha = 0.6,
                       color = "grey70",
                       size = 2) +
-            geom_line(data = data1, aes(elements,
+            ggplot2::geom_line(data = data1, ggplot2::aes(elements,
                                         Median,
                                         color = !! quo_group,
                                         group = !! quo_group),
                       size = 2.5) +
-            facet_wrap(quo_group, ncol = ncol) +
-            scale_y_log10(labels = prettyNum) +
-            scale_fill_viridis(discrete = TRUE) +
-            scale_color_viridis(discrete = TRUE) +
-            theme_dark() +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-            labs(x = "")
+            ggplot2::facet_wrap(quo_group, ncol = ncol) +
+            ggplot2::scale_y_log10(labels = prettyNum) +
+            ggplot2::scale_fill_viridis(discrete = TRUE) +
+            ggplot2::scale_color_viridis(discrete = TRUE) +
+            ggplot2::theme_dark() +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)) +
+            ggplot2::labs(x = "")
 
     } else {
 
         By_Gather_Ribbon(data = data,
                          group = group,
-                         columns = columns,
+                         elements = elements,
                          levels = levels) %>%
-            ggplot(aes(group = ID,
+            ggplot2::ggplot(ggplot2::aes(group = ID,
                        color = !! quo_group)) +
-            geom_ribbon(aes(ymin = min,
+            ggplot2::geom_ribbon(ggplot2::aes(ymin = min,
                             ymax = max,
                             x = elements,
                             fill = !! quo_group,
                             color = !! quo_group),
                         linetype = "blank",
                         alpha = 0.2) +
-            geom_line(data = data2, aes(elements,
+            ggplot2::geom_line(data = data2, ggplot2::aes(elements,
                                         Median,
                                         group = XXX,
                                         color = XXX),
                       alpha = 0.6,
                       size = 1) +
-            geom_line(aes(elements,
+            ggplot2::geom_line(ggplot2::aes(elements,
                           median,
                           color = !! quo_group,
                           group = !! quo_group),
                       size = 2) +
-            facet_wrap(quo_group, ncol = ncol) +
-            scale_y_log10(labels = prettyNum) +
-            scale_fill_viridis(discrete = TRUE) +
-            scale_color_viridis(discrete = TRUE) +
-            theme_dark() +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-            labs(x = "", y = "")
+            ggplot2::facet_wrap(quo_group, ncol = ncol) +
+            ggplot2::scale_y_log10(labels = prettyNum) +
+            ggplot2::scale_fill_viridis(discrete = TRUE) +
+            ggplot2::scale_color_viridis(discrete = TRUE) +
+            ggplot2::theme_dark() +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)) +
+            ggplot2::labs(x = "", y = "")
     }
 }
