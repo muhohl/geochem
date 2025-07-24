219b20451cbda5d02e3e72e4c573a5375dfab302
muhohl <max.hohl@protonmail.com>
2025-07-23 18:13:24 -0700

    Added functions that allow to filter for outliers.

---
+++ R/filter_quantiles.R
@@ -0,0 +1,58 @@
+
+#' filter_quantiles
+#'
+#' Replaces outliers with NAs.
+#'
+#' @param data
+#' Provide data frame in tibble format.
+#' @param .cols
+#' Specify numeric columns that should be screened for NAs.
+#' @param probs
+#' Sequence that determines step size for quantiles.
+#' @param quantile_position
+#' Position in the sequence determined by probs. Values within quantiles higher than
+#' quantile_position will be replaced by NA
+#'
+#' @returns
+#' data
+#' @export
+#' @examples
+
+filter_quantiles <- function(
+  data,
+  .cols,
+  probs = seq(0, 1, 0.05),
+  quantile_position = 20
+) {
+  data_095 <- data |>
+    dplyr::select({{ .cols }}) |>
+    dplyr::mutate(dplyr::across(dplyr::everything(), \(x) {
+      stats::quantile(x, na.rm = TRUE, probs = probs)[[quantile_position]]
+    })) |>
+    dplyr::distinct()
+
+  data_quantiles <- tibble::tibble(ref = 1:nrow(data))
+
+  for (element in .cols) {
+    data_filtered <- data |>
+      dplyr::mutate(ref = 1:dplyr::n()) |>
+      dplyr::filter(
+        !!dplyr::sym(element) <= data_095 |> dplyr::pull(element)
+      ) |>
+      dplyr::select(ref, element)
+    data_quantiles <- dplyr::left_join(data_quantiles, data_filtered)
+  }
+
+  data_quantiles <- data_quantiles |>
+    dplyr::filter(dplyr::if_any(.cols = dplyr::everything(), ~ !is.na(.)))
+
+  data_new <- dplyr::left_join(
+    data |>
+      dplyr::mutate(ref = 1:dplyr::n()) |>
+      dplyr::select(-.cols),
+    data_quantiles
+  ) |>
+    dplyr::select(-ref)
+
+  return(data_new)
+}
